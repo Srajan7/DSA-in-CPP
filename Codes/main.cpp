e730d713fb170e767bb44f1df267009eae28558f
Srajan7 <srajanrai@outlook.com>
2022-04-25 17:31:16 +0530

    25

---
+++ Codes/main.cpp
@@ -0,0 +1,19 @@
+#include "base.h"
+
+int main() {
+    clear();
+
+    cout << "Hello World\n";
+    // Build- g++ main.cpp -o main 
+    // Run- .\main.exe
+
+    // short int 2 bytes long int 8 bytes
+
+    
+
+
+
+
+
+    return 0;
+}
